ef2d8840e8850f19b6c49976418a09b00add4601
ayman <aymane.ahafid@efrei.net>
2024-12-20 13:16:00 +0100

    a

--- epreuve_hasard.py
+++ epreuve_hasard.py
@@ -2,8 +2,29 @@ import random
 import string
 from fonctions_utiles import *
 
-def epreuve_hasard():
-    print("Vous avez choisi une épreuve de hasard!")
+def jeu_lance_des():
+    print("Vous allez jouer au jeu de lance de dès")
+    for tentative in range(3):
+        print(f"Tentative {tentative + 1}: ")
+        print("Tour du joueur")
+        input("Appuyez sur Entrée pour lancer les dès")
+        des_joueur = (random.randint(0, 6), random.randint(0, 6))
+        print(f"Dès obtenue par le joueur : {des_joueur}")
+        if des_joueur[0] == 6 or des_joueur[1] ==6:
+            return True
+        print("Aucune dès de 6 obtenue. On passe au prochain essai")
+        print("Tour du maître du jeu")
+        des_maitre = (random.randint(0, 6), random.randint(0, 6))
+        print(f"Dès obtenue par le maître du jeu : {des_joueur}")
+        if des_maitre[0] == 6 or des_maitre[1] ==6:
+            return False
+        if tentative != 2:
+            print("Aucune dès de 6 obtenue. On passe au prochain essai")
+    print("Après trois essais, aucun des joueurs n'a gagné")
+    return False
+
+
+def bonneteau():
     choix = random.choice(['A', 'B', 'C'])
     print("Devinez sous quel gobelet (A, B ou C) se cache la clé.")
     for tentative in range(2):
@@ -13,4 +34,12 @@ def epreuve_hasard():
             return True
     print(f"Dommage! La clé était sous le gobelet {choix}.")
     return False
+
+def epreuve_hasard():
+    print("Vous avez choisi une épreuve de hasard!")
+    rng = random.randint(1, 2)
+    if rng == 1:
+        jeu_lance_des()
+    elif rng == 2:
+        bonneteau()
 
